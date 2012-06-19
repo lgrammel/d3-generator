e6e785fd860255030a92fc7ddb7986f8bea8bddb
Lars Grammel <lars.grammel@gmail.com>
2012-06-19 10:40:20 -0700

    #15 tracking script compression

--- config/mixpanel-test.js
+++ config/mixpanel-test.js
@@ -1,13 +1,1 @@
-<script type="text/javascript">
-(function(d,c){var a,b,g,e;a=d.createElement("script");a.type="text/javascript";
-    a.async=!0;a.src=("https:"===d.location.protocol?"https:":"http:")+
-    '//api.mixpanel.com/site_media/js/api/mixpanel.2.js';b=d.getElementsByTagName("script")[0];
-    b.parentNode.insertBefore(a,b);c._i=[];c.init=function(a,d,f){var b=c;
-    "undefined"!==typeof f?b=c[f]=[]:f="mixpanel";g=['disable','track','track_pageview',
-    'track_links','track_forms','register','register_once','unregister','identify',
-    'name_tag','set_config'];
-    for(e=0;e<g.length;e++)(function(a){b[a]=function(){b.push([a].concat(
-    Array.prototype.slice.call(arguments,0)))}})(g[e]);c._i.push([a,d,f])};window.mixpanel=c}
-)(document,window.mixpanel||[]);
-mixpanel.init("d5f329e2b61bfd4800deaefd97d31290");
-</script>
+<script type="text/javascript">(function(a,b){var c,d,e,f;c=a.createElement("script"),c.type="text/javascript",c.async=!0,c.src=("https:"===a.location.protocol?"https:":"http:")+"//api.mixpanel.com/site_media/js/api/mixpanel.2.js",d=a.getElementsByTagName("script")[0],d.parentNode.insertBefore(c,d),b._i=[],b.init=function(a,c,d){var h=b;"undefined"!=typeof d?h=b[d]=[]:d="mixpanel",e=["disable","track","track_pageview","track_links","track_forms","register","register_once","unregister","identify","name_tag","set_config"];for(f=0;f<e.length;f++)(function(a){h[a]=function(){h.push([a].concat(Array.prototype.slice.call(arguments,0)))}})(e[f]);b._i.push([a,c,d])},window.mixpanel=b})(document,window.mixpanel||[]),mixpanel.init("d5f329e2b61bfd4800deaefd97d31290")</script>
