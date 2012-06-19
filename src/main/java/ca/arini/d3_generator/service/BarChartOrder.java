2f061b8940c2dca38d86d8399af197be55ba2eba
Lars Grammel <lars.grammel@gmail.com>
2012-06-19 10:59:41 -0700

    added Arini header

--- src/main/java/ca/arini/d3_generator/service/BarChartOrder.java
+++ src/main/java/ca/arini/d3_generator/service/BarChartOrder.java
@@ -1,3 +1,22 @@
+/* ***********************************************************************
+ * 
+ * ARINI CONFIDENTIAL
+ * __________________
+ * 
+ *  Copyright Arini Software Inc. 
+ *  All Rights Reserved.
+ * 
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Arini Software Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Arini Software Inc.
+ * and its suppliers and may be covered by Canadian and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Arini Software Inc.
+ *
+ *************************************************************************/
 package ca.arini.d3_generator.service;
 
 public enum BarChartOrder {
