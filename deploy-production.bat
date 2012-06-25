ad2a1b6ea0f974f7a336193cac7b9e17b5fb52a7
Lars Grammel <lars.grammel@gmail.com>
2012-06-25 11:14:25 -0700

    added scripts to automate deployment

---
+++ deploy-production.bat
@@ -0,0 +1,5 @@
+git checkout production
+git merge staging
+git push github production
+git push production production:master
+git checkout master
---
+++ deploy-staging.bat
@@ -0,0 +1,5 @@
+git checkout staging
+git merge master
+git push github staging
+git push staging staging:master
+git checkout master
