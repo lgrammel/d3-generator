245beafa2c934cc07a3bafeb1e3799264c95eebd
Lars Grammel <lars.grammel@gmail.com>
2012-06-27 20:13:15 -0700

    improved error handling

--- config/errorhandler-logging.js
+++ config/errorhandler-logging.js
@@ -1,7 +1,8 @@
 errorHandler = {
-    onError: function() {
-        console.log("onError");
-        console.log("this=" + JSON.stringify(this, null, 2));
-        console.log("arguments=" + JSON.stringify(arguments, null, 2));
+    onError: function(error, message) {
+        if (message !== undefined) {
+            console.log(message);
+        }
+        console.log(error);
     }
 };
